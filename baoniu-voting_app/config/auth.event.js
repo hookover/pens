ab49b65cd53fa6d7c8466e74414c1e07cac1cf01
Mr.Chen <688977@qq.com>
2016-06-28 20:05:50 +0800

    delete

--- baoniu-voting_app/config/auth.event.js
+++
@@ -1,8 +0,0 @@
-// change the file name to auth.js
-module.exports = {
-    githubAuth: {
-        clientID: '',
-        clientSecret: '',
-        callbackURL: 'http://localhost:3000/auth/github/callback',
-    }
-};
